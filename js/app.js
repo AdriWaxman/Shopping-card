aeab1475ce2ee4601e1bd5e451ceea388f2b5b84
franGomezNavas <frangomeznavas@gmail.com>
2018-04-16 18:00:33 +0200

    nuevo

--- js/app.js
+++ js/app.js
@@ -1,34 +1,78 @@
-var Item = function(name, price){
-	this.name = name;
-	this.picture = picture;
-	this.description = description;
-	this.price = price;
+// var Item = function(name, price){
+// 	this.name = name;
+// 	this.picture = picture;
+// 	this.description = description;
+// 	this.price = price;
+// }
 
-}
+// var ShoppingCart = function(list, totalPay){
+// 	this.list = [];
+// 	this.totalPay = totalPay;
+// }
 
-var ShoppingCart = function(list, totalPay){
-	this.list = [];
-	this.totalPay = totalPay;
-}
+// ShoppingCart.prototype.addItem = function(itemAdd, quantity){
+// 	var product = {};
+// 			product.itemAdd = itemAdd;
+// 			product.quantity = quantity;
+// 			this.list.push(product);
+// }
 
-ShoppingCart.prototype.addItem = function(itemAdd, quantity){
-	var product = {};
-			product.itemAdd = itemAdd;
-			product.quantity = quantity;
-			this.list.push(product);
-}
+// ShoppingCart.prototype.totalCost = function(Item, discount){
+// 	// var total = 
+// }
 
-ShoppingCart.prototype.totalCost = function(Item, discount){
-	// var total = 
-}
-
-var apple = new Item ('Apple', 10);
-var orange = new Item ('Orange', 5);
+// var apple = new Item ('Apple', 10);
+// var orange = new Item ('Orange', 5);
 
 
-var cart = new ShoppingCart();
-cart.addItem (apple,1);
-cart.addItem (orange,1);
+// var cart = new ShoppingCart();
+// cart.addItem (apple,1);
+// cart.addItem (orange,1);
+
+
+// =====   INTENTO COMO LOS FUTBOLISTAS ==== //
+
+$(document).ready(function(){
+
+    var main = $('#products');
+
+    $.ajax({
+        url: 'js/shoppingCardApi.json',
+        type: 'GET',
+        dataType: 'JSON',
+        success: function(response){
+                if(response.success){
+                var tshirt = response.shoppingCardApi;
+
+                for(var i = 0; i < shoppingCardApi.length; i++){
+
+                    var listItem =
+                    '<div class="item">' +
+                        '<div class="row">' +
+                            '<div class="col-md-3">' +
+                                '<img src="' + shoppingCardApi[i].picture + '" class="footballer-img">' +
+                            '</div>' +
+                            '<div class="col-md-8">' +
+                                '<div class="footballer-details">' +
+                                    '<p class="name"><strong>Name:</strong>' + shoppingCardApi[i].name + '</p>' +
+                                    '<p class="club"><strong>Club:</strong>' + shoppingCardApi[i].price + '</p>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>';
+
+                    main.append(listItem);
+                }
+            } else {
+                console.log("ha habido algún problema con los datos remotos")
+            }   
+        }
+    })
+})
+
+
+
+
 
 
 
